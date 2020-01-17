fd3f101a35d15f7a668149f5037744dd8f1b95ba
吴楠 <50066750+WuNanya@users.noreply.github.com>
2020-01-17 19:01:27 +0800

    Update wsgi.py

--- Book_system_ui/wsgi.py
+++ Book_system_ui/wsgi.py
@@ -3,8 +3,7 @@ WSGI config for Book_system_ui project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+
 """
 
 import os
